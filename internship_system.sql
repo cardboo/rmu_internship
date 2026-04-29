876a42a755da0472ca6388da4ed5732ddc34d32d
SelormAK <111173850+SelormAK@users.noreply.github.com>
2026-04-29 14:37:42 +0000

    new sql db

--- internship_system.sql
+++ internship_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2026 at 09:01 PM
+-- Generation Time: Apr 29, 2026 at 04:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
